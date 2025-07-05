34304c1d84e82cae3319992bef8cc60ce0a5fe7c
AntoRios <adelosrios53@gmail.com>
2025-07-05 20:36:00 +0200

    First Commit Proof

---
+++ ex13.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 19:08:15 2025
+
+@author: Antonio
+"""
+#ex13.py
+# Parameters, Unpacking. Variables
+
+from sys import argv
+
+script, first, second, third = argv      # unpacks variables
+
+print ('the script is called:', script)
+print ('your first variable is:', first)
+print ('your second  variable is:', second)
+print ('your third variable is:', third)
+
+
+
+
+
+import sys
+print(sys.executable)

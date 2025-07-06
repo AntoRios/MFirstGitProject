58e5cf57b17d5cdb5ef3a5fc24e58bcb12ded076
AntoRios <adelosrios53@gmail.com>
2025-07-06 19:40:56 +0200

    Added Featuresgit status

--- ex13.py
+++ ex13.py
@@ -9,12 +9,13 @@ Created on Mon Jan 27 19:08:15 2025
 
 from sys import argv
 
-script, first, second, third = argv      # unpacks variables
+script, first, second, third, fourth = argv      # unpacks variables
 
 print ('the script is called:', script)
 print ('your first variable is:', first)
 print ('your second  variable is:', second)
 print ('your third variable is:', third)
+print ('your fourth variable is:', fourth)
 
 
 
